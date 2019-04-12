5679098db0a8113f620f254cb73f1a8d8a7b9c8b
xcy <2698064198@qq.com>
2019-04-12 15:02:01 +0800

    套接字

--- days0410/demo3.py
+++ days0410/demo3.py
@@ -24,17 +24,20 @@ BUFFER_SIZE = 1024
 
 def tsend():
     while True:
-        ADDRES_TO = ('192.168.12.167', 60000)
+        sendport = int(input('请输入要发送的对象：'))
+        ADDRES_TO = ('192.168.12.167', sendport)
         time.sleep(2)
-        clientsocket.sendto('hello'.encode('utf8'), ADDRES_TO)
+        send_content = input('请输入要发送的内容')
+        clientsocket.sendto(send_content.encode('utf8'), ADDRES_TO)
 
 
 def trece():
     BUFFER_SIZE = 1024
     while True:
         cont, info = clientsocket.recvfrom(BUFFER_SIZE)
-        print(cont.decode('utf8'))
-        print(info)
+        # print(cont.decode('utf8'))
+        # print(info)
+        print('收到%s发来的消息：%s' % (info[1], cont.decode('utf8')))
 
 
 if __name__ == '__main__':
--- days0410/demo4.py
+++ days0410/demo4.py
@@ -10,20 +10,23 @@ def recv():
     while True:
         time.sleep(1)
         cont, info =server_socket.recvfrom(1024)
-        print(cont.decode('utf8'), info)
-        client_info.append(info)
+        print('收到%s发来的消息：%s' % (info[1], cont.decode('utf8')))
+        # print(cont.decode('utf8'), info)
+        # client_info.append(info)
         # client_info[0] = info
         # print(client_info)
 
 
 def send():
     while True:
+        sendport = int(input('请输入要发送的对象：'))
+        sendinfo = ('192.168.12.167', sendport)
         send_content = input('请输入发送内容：')
-        server_socket.sendto(send_content.encode('utf8'), (client_info[-1]))
+        server_socket.sendto(send_content.encode('utf8'), sendinfo)
 
 
 if __name__ == '__main__':
-    client_info = []
+    # client_info = []
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind(('192.168.12.167', 60000))
     t1 = threading.Thread(target=recv)
